4cd6c99cb41c7f4495152eb0cbb2df58db24b39a
Keith <kpost342005@hotmail.com>
2025-12-13 10:36:41 -0500

    Calculated and displayed mean target variable values by new Boolean fields and moved pairwise testing to archived script

--- code/_02_feat-eng_eda.py
+++ code/_02_feat-eng_eda.py
@@ -9,9 +9,6 @@ import matplotlib.pyplot as plt
 import statsmodels.api as sm
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import FunctionTransformer
-import itertools
-from sklearn.metrics import matthews_corrcoef
-from scipy.stats import chi2_contingency
 
 
 ## Change wd
@@ -33,7 +30,7 @@ from _00_helper_objs_fns import (
   make_barplot,
   map_avg_price,
   log_transform,
-  correlation_ratio
+  create_boolean_desc_features
 )
 
 
@@ -108,11 +105,33 @@ plt.close()
 
 
 ## Matching keywords in item_description
+### Create Boolean fields 
+df = create_boolean_desc_features(df)
 df['has_keyword_new'] = df['item_description'].str.contains('new', case=False, na=False)
-labs_new = ['No', 'Yes']
+
+
+### Assess how they differ
+#means
+df.groupby('has_keyword_new_like', as_index=False)['price_log'].mean()
+df.groupby('has_keyword_used_like', as_index=False)['price_log'].mean()
+df.groupby('has_keyword_authentic_like', as_index=False)['price_log'].mean()
+df.groupby('has_keyword_rare_like', as_index=False)['price_log'].mean()
+df.groupby('has_keyword_set_like', as_index=False)['price_log'].mean()
+
+df.groupby('has_keyword_new', as_index=False)['price_log'].mean()
+
+#plots
+sns.barplot(data=df, x='has_keyword_new', y='price_log')
+plt.show()
+plt.close()
+
+sns.barplot(data=df, x='has_keyword_new_like', y='price_log')
+plt.show()
+plt.close()
 
 
 ### Univariate plot
+labs_new = ['No', 'Yes']
 ax = sns.countplot(data=df, x='has_keyword_new', color=fixed_colors['has_keyword_new'])
 ax.set_xticklabels(labs_new)
 ax.set_xlabel("Has keyword 'new'?")
@@ -220,171 +239,11 @@ len(redundant_cols) #0
 
 
 ## Functional redundancy or strong collinearity
-### Remove extraneous fields for assessing collinearity (28 to start)
-cols_to_drop = ['train_id', 'name', 'category_name', 'brand_name', 'price', 'item_description',
-                'department', 'category', 'class', 'desc_wc', 'name_len', 'price_log']
-              
-df_dropped = df.drop(cols_to_drop, axis=1)
-df_dropped.info() #17
-
-
-### Split data into separate types
-#numeric (7)
-cols_num = ['dpt_top5_lprice', 'cat_top5_lprice', 'class_top5_lprice', 'brand_top5_lprice',
-            'item_condition_id_lprice', 'name_wc', 'desc_len']
-df_num = df[cols_num]
-
-#binary (5)
-cols_bi = ['shipping', 'has_cat_name', 'has_brand', 'has_desc', 'has_keyword_new']
-df_bi = df[cols_bi].astype(int) #convert Boolean to integer type
-
-#categorical (4)
-cols_cat = ['dpt_top5', 'cat_top5', 'class_top5', 'brand_top5']
-df_cat = df[cols_cat]
-
-#ordinal (1)
-cols_ord = ['item_condition_id']
-df_ord = df[cols_ord]
-
-
-### Numeric-numeric: Spearman correlation
-#run correlation
-df_corr = df_num.corr('spearman').abs() 
-
-#test for # values per column = 1 (expect 1, diagonal)
-df_corr.apply(lambda x: sum(x == 1), axis=1).unique() #all 1s
-
-#replace 1s with 0s
-df_corr_replace = df_corr.replace(1, 0)
-
-#assess for strong pairwise correlations
-df_corr_replace.apply(lambda x: sum(x > 0.9), axis=1).unique() 
-#Result: all 0s, so none
-
-
-#### Binary-binary: Phi coefficient (equivalent to Pearson)
-#create empty corr matrix
-cols_bi = df_bi.columns
-phi_matrix = pd.DataFrame(np.eye(len(cols_bi)), index=cols_bi, columns=cols_bi)
-
-#compute pairwise MCC (phi)
-for col1, col2 in itertools.combinations(cols_bi, 2):
-    phi = matthews_corrcoef(df_bi[col1], df_bi[col2])
-    phi_matrix.loc[col1, col2] = phi
-    phi_matrix.loc[col2, col1] = phi  
-
-#convert to abs
-phi_matrix = phi_matrix.abs()
-
-#test for # values per column = 1 (expect 1, diagonal)
-phi_matrix.apply(lambda x: sum(x == 1), axis=1).unique() #all 1s
-
-#replace 1s with 0s
-phi_matrix_replace = phi_matrix.replace(1, 0)
-
-#assess for strong pairwise correlations
-phi_matrix_replace.apply(lambda x: sum(x > 0.8), axis=1).unique() 
-#Result: all 0s, so none
-
-
-### Ordinal-ordinal: Spearman correlation
-#only one ordinal feature (so no pairs to assess)
-
-
-### Numeric-binary: Pearson correlation
-df_num_bi = None #start with empty DF
-
-#iterate .corrwith to calculate Pearson corrs
-for col in df_bi.columns:
-  df_corr = df_num.corrwith(df_bi[col]).to_frame(col).abs()
-  df_num_bi = pd.concat([df_num_bi, df_corr], axis=1)
-
-df_num_bi
-
-#test for # values per column = 1
-df_num_bi.apply(lambda x: sum(x == 1), axis=1).unique() #all 0s, so none
-
-#assess for strong pairwise correlations
-df_num_bi.apply(lambda x: sum(x > 0.9), axis=1).unique() 
-#Result: all 0s, so none
-
-
-### Numeric-categorical: correlation ratio
-#combine DFs
-df_cat_num = pd.concat([df_cat, df_num], axis=1)
-
-#build DF of corr_ratios (start with empty DF with index and col names)
-df_corr_ratio = pd.DataFrame(index=cols_cat,
-                             columns=cols_num)
-
-#iterate through index and cols to populate DF
-for cat in cols_cat:
-  for num in cols_num:
-    corr_ratio = correlation_ratio(df_cat_num[cat], df_cat_num[num])
-    df_corr_ratio.loc[cat, num] = corr_ratio
-
-df_corr_ratio
-
-
-#test for # values per column near 1
-df_corr_ratio.apply(lambda x: sum(x > 0.999999), axis=1).unique() 
-#all 1s (expected); because the log-price per top 5 cat are deterministic, they will be retained
-
-#assess for strong pairwise correlations
-df_corr_ratio.apply(lambda x: sum(x >= 0.5), axis=1)
-#3 (2 more than expected) for cat_top5 and 1 for others
-
-df_corr_ratio.loc['cat_top5']
-#Result: cat_top5 is strongly associated with dpt_top5_lprice and class_top5_lprice
-#--> because of this and the 1s (expected), this will be dropped for linear models
-
-
-### Categorical-categorical and categorical-binary: Cramer's V
-#combine DFs
-df_cat_bi = pd.concat([df_cat, df_bi], axis=1)
-
-#create empty matrix
-cols_cat_bi = df_cat_bi.columns
-cramers_cat_bi_matrix = pd.DataFrame(np.eye(len(cols_cat_bi)), index=cols_cat_bi, 
-                                     columns=cols_cat_bi)
-
-#compute pairwise Cramer's V
-for col1, col2 in itertools.combinations(cols_cat_bi, 2):
-    #build a contingency table
-    confusion_matrix = pd.crosstab(df_cat_bi[col1], df_cat_bi[col2])
-    #computes the Chi-squared stat
-    chi2 = chi2_contingency(confusion_matrix)[0]
-    #n = number of obs
-    n = confusion_matrix.sum().sum()
-    #k = smaller dim of the contingency table
-    k = min(confusion_matrix.shape)
-    cramers_v = np.sqrt(chi2/(n*(k-1)))
-    
-    cramers_cat_bi_matrix.loc[col1, col2] = cramers_v
-    cramers_cat_bi_matrix.loc[col2, col1] = cramers_v
-
-cramers_cat_bi_matrix
-
-#test for # values per column = 1 (expect 1, diagonal)
-cramers_cat_bi_matrix.apply(lambda x: sum(x == 1), axis=1).unique() #all 1s
-
-#replace 1s with 0s
-cramers_cat_bi_matrix_replace = cramers_cat_bi_matrix.replace(1, 0)
-
-#assess for strong pairwise correlations
-cramers_cat_bi_matrix_replace.apply(lambda x: sum(x > 0.9), axis=1).unique() 
-#Result: all 0s, so none
-
-
-
-### Ordinal-numeric: Spearman correlation
-df_ord_num_corr = (
-  df_num.corrwith(df_ord['item_condition_id'], 
-                  method='spearman').to_frame('item_condition_id')
-                                    .abs()
-  )
-len(df_ord_num_corr[df_ord_num_corr['item_condition_id']>0.9])
-#Result: 0, so none is greater than 0.9
+# pd.to_pickle(df, "train_collinear_test.pkl")
+# see archive for details on some pairwise correlational tests
+#note: ultimately did not weight heavily because LM was dropped as a prospective model which is
+  #greatly affected by collinearity and with TF-IDF would require an iterative drop-refit process
+  
 
 
 #4. Categorical variables
@@ -397,7 +256,9 @@ len(df_ord_num_corr[df_ord_num_corr['item_condition_id']>0.9])
 cols_mod_full = ['train_id', #id
                  'name', 'item_description', #string (for tf-idf)
                  'item_condition_id', #ordinal
-                 'shipping', 'has_cat_name', 'has_brand', 'has_desc', 'has_keyword_new', #Bi/Bool
+                 'shipping', 'has_cat_name', 'has_brand', 'has_desc', #'has_keyword_new', #Bi/Bool
+                 'has_keyword_new_like', 'has_keyword_authentic_like', 'has_keyword_used_like',
+                 'has_keyword_rare_like', 'has_keyword_set_like',
                  'name_wc', 'desc_len', #num
                  'department', 'category', 'class', 'brand_name', #categorical
                  'price', 'price_log'] #target
@@ -406,7 +267,9 @@ df_mod_full=df[cols_mod_full]
 
 
 ## Convert Boolean cols to integers
-cols_bool = ['has_cat_name', 'has_brand', 'has_desc', 'has_keyword_new']
+cols_bool = ['has_cat_name', 'has_brand', 'has_desc', 'has_keyword_new_like',
+'has_keyword_authentic_like', 'has_keyword_used_like', 'has_keyword_rare_like', 
+'has_keyword_set_like']
 df_mod_full[cols_bool] = df_mod_full[cols_bool].astype(int)
 
 
